6be7c0f6cac02fbfdd546da06db8442b61868bef
rishabhpoddar <rishabh.poddar@gmail.com>
2025-05-08 15:29:22 +0530

    removes unnecessary function

--- utils.ts
+++ utils.ts
@@ -1,22 +1,5 @@
 import express from "express";
 
-function getAndAssertCorrectVersion(req: express.Request, allowedVersions: string[]): string {
-    let version = req.params.version;
-    if (!version) {
-        throw {
-            status: 400,
-            message: 'Missing version parameter in path'
-        };
-    }
-    if (!allowedVersions.includes(version)) {
-        throw {
-            status: 400,
-            message: 'version parameter must be one of the allowed versions: ' + allowedVersions.join(', ')
-        };
-    }
-    return version;
-}
-
 export function apiHandler(handler: (req: express.Request, res: express.Response, next: express.NextFunction) => Promise<void>, requireAuth: boolean) {
     return async (req: express.Request, res: express.Response, next: express.NextFunction) => {
         try {
